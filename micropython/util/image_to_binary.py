995d3f2668097ba13401094661f3ee42181bdd89
Edward Evans <elevans2@wisc.edu>
2022-10-08 13:50:45 -0500

    Update image_to_binary script
    The script now takes terminal arguments.
    
    python image_to_binary.py -i "input.pbm" -o "output.bin"
    
    Process your image into a 1-bit indexed image (i.e. PBM format).
    This script extracts the byte stream of the image and returns a slightly
    smaller bin file. Extracting the byte stream from the .PBM file
    here means the microcontroller wont have to do it, saving CPU cycles.
    This also allows you to optionally invert the image.

--- micropython/util/image_to_binary.py
+++ micropython/util/image_to_binary.py
@@ -1,28 +1,44 @@
-from PIL import Image
+import getopt, sys
 
-def convert_image_to_bytearray(path: str) -> bytearray:
+def convert_image_to_bytearray(path: str, invert=True) -> bytearray:
     """
-    Ported from this online post:
-    https://forum.micropython.org/viewtopic.php?f=16&t=2974
+    Extract the byte stream from a .PBM image file. Optionally, 
+    invert the byte stream for OLED displays.
     """
-    image = Image.open(path)
-    buffer = bytearray((image.height // 8) * image.width)
-    i = 0
-    for y in range(image.height // 8):
-        for x in range(image.width):
-            byte = 0
-            for bit in range(8):
-                pixel = image.getpixel((x, y * 8 + bit))
-                if pixel == 255:
-                    byte |= (1 << bit)
-            buffer[i] = byte
-            i += 1
+    with open(path, "rb") as f:
+        f.readline() # magic number
+        f.readline() # creator comment
+        f.readline() # image dimensions
+        buffer = bytearray(f.read())
     
-    return buffer
+    # XOR bits for OLED displays
+    if invert:
+        for i in range(len(buffer)):
+            buffer[i] ^= 0xFF
+
+    return buffer 
 
 def write_to_file(buffer: bytes, file_name: str):
     """
-    Write buffer to a binary file.
+    Write a bytearray/buffer to a binary file.
     """
     with open(file_name, "wb") as f:
         f.write(buffer)
+
+args_list = sys.argv[1:]
+options = "hi:o:"
+long_options = ["help", "input=", "output="]
+try:
+    # parse arguments
+    args, value = getopt.getopt(args_list, options, long_options)
+    # check for each argument
+    for current_arg, current_value in args:
+        if current_arg in ("-h", "--help"):
+            print("help")
+        if current_arg in ("-i", "--input"):
+            buffer = convert_image_to_bytearray(current_value)
+        if current_arg in ("-o", "--output"):
+            write_to_file(buffer, current_value)
+
+except getopt.error as err:
+    print(str(err))
