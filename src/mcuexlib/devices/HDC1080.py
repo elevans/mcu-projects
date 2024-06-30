8f5923542288d0b09b7e64a55508a49c56f135d4
Edward Evans <elevans2@wisc.edu>
2024-06-30 13:28:57 -0500

    Refactor sensors modules into devices

--- src/mcuexlib/devices/HDC1080.py
+++ src/mcuexlib/devices/HDC1080.py
@@ -1,10 +1,6 @@
 import time
 from micropython import const
 
-# set constants
-BH1750FVI_POWER_OFF = const(0x00)
-BH1750FVI_POWER_ON = const(0x01)
-BH1750FVI_RESET = const(0x07)
 HDC1080_ADDRESS = const(0x40)
 HDC1080_TEMP_REG = const(0x00)
 HDC1080_HUM_REG = const(0x01)
@@ -15,18 +11,6 @@ HDC1080_DEVICE_ID = const(0xFF)
 HDC1080_SERIAL_ID_FIRST = const(0xFB)
 HDC1080_SERIAL_ID_MID = const(0xFC)
 HDC1080_SERIAL_ID_LAST = const(0xFD)
-GENERIC_STOP = const(0xFF)
-
-class BH1750FVI:
-    """
-    """
-    def __init__(self, i2c):
-        self.i2c = i2c
-        self._init_device()
-        return
-
-    def _init_device(self):
-        self.i2c.writeto_mem()
 
 class HDC1080:
     """
@@ -39,7 +23,6 @@ class HDC1080:
         self._t_reg_buf = bytes([HDC1080_TEMP_REG])
         self._h_reg_buf = bytes([HDC1080_HUM_REG])
         self._init_device()
-        return
 
     def _init_device(self):
         """
@@ -54,34 +37,37 @@ class HDC1080:
             bytes([HDC1080_CONFIG])
             )
 
-    def get_temp(self, unit="c"):
-        """
-        Get temperature measurement in Celcius or Fahrenheiht.
+    def read_temp(self, unit="c"):
+        """Read the temperature.
+
+        Read the temperature and return in Celcius, Fahrenheit or the raw
+        value.
+
+        :param unit: Set the unit of the temperature read
+        :return: Temperature in specified unit
         """
         # point to temperature register
         self.i2c.writeto(HDC1080_ADDRESS, self._t_reg_buf)
         time.sleep(0.015)
-        value = int.from_bytes(self.i2c.readfrom(HDC1080_ADDRESS, 2), "big")
+        v = int.from_bytes(self.i2c.readfrom(HDC1080_ADDRESS, 2), "big")
         if unit == "c":
-            return (value / 2 ** 16) * 165 - 40
+            return (v / 2 ** 16) * 165 - 40
         elif unit == "f":
-            return (value * 1.8 / (2 ** 16)) * 165 - 40
+            return (v * 1.8 / (2 ** 16)) * 165 - 40
         else:
-            return None
+            return v
 
-    def get_hum(self):
-        """
-        Get relative humidity measurement.
+    def read_hum(self):
+        """Read the relative humidity.
+
+        Read the relatively humidity as a percentage.
+
+        :return: Relative humidity percent
         """
         # point to humidity register
         self.i2c.writeto(HDC1080_ADDRESS, self._h_reg_buf)
         time.sleep(0.015)
         value = int.from_bytes(self.i2c.readfrom(HDC1080_ADDRESS, 2), "big")
+        
         return (value / 2 ** 16) * 100
 
-class LSM303DLHC:
-    """
-    """
-    def __init__(self, i2c):
-        self.i2c = i2c
-        return
