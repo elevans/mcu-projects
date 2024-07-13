981fe66107969b0846a6dd4bb4870547daa78709
Edward Evans <elevans2@wisc.edu>
2024-07-13 11:24:08 -0500

    Clean up i2c display and button example

--- examples/i2c_display.py
+++ examples/i2c_display.py
@@ -8,9 +8,12 @@ DSP_HEIGHT = const(32)
 DSP_WIDTH = const(128)
 
 # config pins
-dsp_scl = Pin(17)
 dsp_sda = Pin(16)
+dsp_scl = Pin(17)
 
 # initialize the display
-dsp = display.SSD1306_I2C(DSP_WIDTH, DSP_HEIGHT, I2C(id=0, scl=dsp_scl, sda=dsp_sda, freq=I2C_FREQ))
+dsp = display.SSD1306_I2C(
+        DSP_WIDTH, DSP_HEIGHT,
+        I2C(id=0, sda=dsp_sda, scl=dsp_scl, freq=I2C_FREQ)
+        )
 dsp.fill(0)
--- examples/push_button_interrupt.py
+++ examples/push_button_interrupt.py
@@ -4,10 +4,10 @@ from utime import sleep_ms
 # config pins
 btn = Pin(14, Pin.IN)
 
-# global push button vars
+# global button vars
 btn_cv = 1 # current value
 btn_lv = 1 # last value
-btn_state = 1 # global button state for ISR
+btn_st = 1 # button state for ISR
 btn_t = 0 # button time counter
 
 def btn_push_interrupt(pin):
@@ -16,7 +16,7 @@ def btn_push_interrupt(pin):
     Detect a button press with a default of
     10 ms delay.
     """
-    global btn_state
+    global btn_st
     global btn_t
 
     cv = pin.value()
@@ -30,15 +30,15 @@ def btn_push_interrupt(pin):
         sleep_ms(1)
     
     # toggle between 0 and 1
-    btn_state = btn_state ^ 1
+    btn_st = btn_st ^ 1
 
 
 # initialize button w/ intterupt
-btn_1.irq(btn_push_interrupt, Pin.IRQ_FALLING)
+btn.irq(btn_push_interrupt, Pin.IRQ_FALLING)
 
 # do something different based on the button state
 while True:
-    if btn_state == 1:
+    if btn_st == 1:
         print("1")
     else:
         print("0")
