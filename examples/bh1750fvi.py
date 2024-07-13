8b4635061a30af3df776880d356e8e5c1b8210ca
Edward Evans <elevans2@wisc.edu>
2024-07-13 11:25:19 -0500

    WIP Add update BH1750 example

--- examples/bh1750fvi.py
+++ examples/bh1750fvi.py
@@ -6,6 +6,8 @@ from micropython import const
 I2C_FREQ = const(400000)
 
 # config pins
+bh1750_sda = Pin()
+bh1750_scl = Pin()
 
 # initialize the BH1750FVI sensor
-BH1750FVI(I2C(id=0, scl=, sda=, freq=), BH1750FVI.ADDR_L)
+BH1750FVI(I2C(id=0,sda=bh1750_sda, scl=bh1750_scl, freq=I2C_FREQ), BH1750FVI.ADDR_L)
